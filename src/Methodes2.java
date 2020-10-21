4b4a2fba91fc31f23002e9c233bcb12c448e8e46
Michiel-PC <michielbos01@gmail.com>
2020-10-21 14:14:42 +0200

    first commit

---
+++ src/Methodes2.java
@@ -0,0 +1,14 @@
+public class Methodes2 {
+    static void HelloWorld() {
+        System.out.println("HelloWorld");
+
+    }
+    static void Groet();{
+        System.out.println("Hallo User");
+
+    }
+    static void Groet(String);{
+        System.out.println("Hallo User");
+
+    }
+}
